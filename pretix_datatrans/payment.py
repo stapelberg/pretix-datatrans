a5bd1a1cea48475791890ecbd8b6c781337e97f5
Michael Stapelberg <michael@stapelberg.de>
2023-05-28 08:05:39 +0200

    implement support for refunds
    fixes https://github.com/stapelberg/pretix-datatrans/issues/1

--- pretix_datatrans/payment.py
+++ pretix_datatrans/payment.py
@@ -3,7 +3,7 @@ import requests
 from django.http import HttpRequest
 from django.template.loader import get_template
 from django.utils.translation import gettext_lazy as _
-from pretix.base.models import Event, OrderPayment
+from pretix.base.models import Event, OrderPayment, OrderRefund
 from pretix.base.payment import BasePaymentProvider, PaymentException
 from pretix.base.settings import GlobalSettingsObject
 from pretix.multidomain.urlreverse import build_absolute_uri
@@ -31,6 +31,51 @@ class Datatrans(BasePaymentProvider):
         template = get_template("pretix_datatrans/payment_confirm.html")
         ctx = {"request": request, "event": self.event, "settings": self.settings}
         return template.render(ctx)
+
+    def payment_refund_supported(self, payment: OrderPayment):
+        return True
+
+    def payment_partial_refund_supported(self, payment: OrderPayment):
+        return True
+
+    def execute_refund(self, refund: OrderRefund):
+        payment_info = refund.payment.info_data
+        if not payment_info:
+            raise PaymentException(_("datatrans: no payment info found"))
+
+        gs = GlobalSettingsObject()
+        # initialize transaction by calling datatrans API
+        refund_url = (
+            "https://api.sandbox.datatrans.com/v1/transactions/"
+            + payment_info["transaction"]
+            + "/credit"
+        )
+        if not gs.settings.payment_datatrans_sandbox:
+            refund_url = refund_url.replace(".sandbox", "")
+
+        logger.info("refund_url = %s" % refund_url)
+
+        # https://api-reference.datatrans.ch/#tag/v1transactions/operation/credit
+        response = requests.post(
+            refund_url,
+            json={
+                "currency": self.event.currency,
+                "refno": refund.full_id,
+                "amount": float(refund.amount) * 100,
+            },
+            auth=HTTPBasicAuth(
+                gs.settings.payment_datatrans_merchant_id,
+                gs.settings.payment_datatrans_api_password,
+            ),
+        )
+        if not response:
+            raise PaymentException(
+                _("datatrans: Fehler %s: %s" % (response.status_code,
+                                                response.content))
+            )
+        refund.done()
+        body = response.json()
+        logger.info("datatrans credit body = %s" % body)
 
     def execute_payment(self, request: HttpRequest, payment: OrderPayment):
         gs = GlobalSettingsObject()
